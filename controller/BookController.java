c0445d5a33dc90f4d90ac5371d9e38307eb844be
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-23 22:44:16 +0300

    Added Getters for Books in BookController

--- controller/BookController.java
+++ controller/BookController.java
@@ -1,15 +1,37 @@
 package com.example.bwa.controller;
 
 import com.example.bwa.service.BookService;
+import com.example.bwa.user.Book;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping()
+@RequestMapping(path = "/book")
 public class BookController {
     @Autowired
     private final BookService bookService;
+
+    @GetMapping(path = "/id/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable() Long id){
+        return new ResponseEntity(bookService.getBookById(id).get(), HttpStatus.OK);//daca e Optional, pui .get()
+    }
+
+    @GetMapping(path = "/name/{name}")
+    public ResponseEntity<List<Book>> getBooksByName(@PathVariable() String name){
+        return new ResponseEntity(bookService.getBooksByName(name), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/books")
+    public ResponseEntity<List<Book>> getBooks(){
+        return new ResponseEntity<>(bookService.getBooks(), HttpStatus.OK);
+    }
+
 }
+
+
