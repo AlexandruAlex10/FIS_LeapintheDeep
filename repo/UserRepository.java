68ff616c49169b58870efd6a6e4ce88e79c261e1
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-24 00:00:47 +0300

    Created an Optional Collection for future use

--- repo/UserRepository.java
+++ repo/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.bwa.repo;
 import com.example.bwa.user.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
