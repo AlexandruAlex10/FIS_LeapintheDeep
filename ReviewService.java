5bf31bd155a697f3dae0723805f5c72c88a137d2
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-24 12:52:55 +0300

    Added a Getter and a Poster for reviews in ReviewService

--- ReviewService.java
+++ ReviewService.java
@@ -1,14 +1,28 @@
 package com.example.bwa.service;
 
 import com.example.bwa.repo.ReviewRepository;
+import com.example.bwa.user.Review;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ReviewService {
     @Autowired
     private final ReviewRepository reviewRepository;
-    
+
+    public List<Review> getReviews() {
+
+        List<Review> allReviews = new ArrayList<>();
+        this.reviewRepository.findAll().forEach(allReviews::add);
+        return allReviews;
+    }
+
+    public Review postReview(Review newReview) {
+        return this.reviewRepository.save(newReview);
+    }
 }
