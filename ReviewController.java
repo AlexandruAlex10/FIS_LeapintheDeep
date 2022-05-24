5e5844695628c37f9549f626369daf04b61d1bff
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-24 12:52:23 +0300

    Added a Getter and a Poster for reviews in ReviewController

--- ReviewController.java
+++ ReviewController.java
@@ -1,9 +1,14 @@
 package com.example.bwa.controller;
 
 import com.example.bwa.service.ReviewService;
+import com.example.bwa.user.Review;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +16,14 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
     @Autowired
     private final ReviewService reviewService;
-    
+
+    @GetMapping
+    public ResponseEntity<List<Review>> getReviews(){
+        return new ResponseEntity<>(this.reviewService.getReviews(), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/thisReview")
+    public ResponseEntity<Review> postReview(@RequestBody Review newReview){
+        return new ResponseEntity<>(this.reviewService.postReview(newReview), HttpStatus.OK);
+    }
 }
