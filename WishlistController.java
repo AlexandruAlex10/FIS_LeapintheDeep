00976deba7c93bfe5ff84cea508d1ee197fb8427
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-24 12:22:58 +0300

    Added a Getter and a Deleter for wishes in WishlistController

--- WishlistController.java
+++ WishlistController.java
@@ -1,9 +1,14 @@
 package com.example.bwa.controller;
 
 import com.example.bwa.service.WishlistService;
+import com.example.bwa.user.Book;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -11,5 +16,15 @@ import org.springframework.web.bind.annotation.*;
 public class WishlistController {
     @Autowired
     private final WishlistService wishlistService;
-    
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getBooks(){
+        return new ResponseEntity<>(this.wishlistService.getBooks(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public HttpStatus deleteBookById(@PathVariable("id") Long id) {
+        this.wishlistService.deleteBookById(id);
+        return HttpStatus.OK;
+    }
 }
