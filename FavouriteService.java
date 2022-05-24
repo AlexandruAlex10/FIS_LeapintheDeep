e237f5c79e038304d4321af13728c996ac178068
AlexandruAlex10 <102253497+AlexandruAlex10@users.noreply.github.com>
2022-05-24 12:18:59 +0300

    Added a Getter and a Deleter for favorites in FavouriteService

--- FavouriteService.java
+++ FavouriteService.java
@@ -1,9 +1,14 @@
 package com.example.bwa.service;
 
 import com.example.bwa.repo.FavouriteRepository;
+import com.example.bwa.user.Book;
+import com.example.bwa.user.Favourite;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,4 +16,23 @@ public class FavouriteService {
     @Autowired
     private final FavouriteRepository favouriteRepository;
 
+    public List<Book> getBooks() {
+        List<Book> allBooks = new ArrayList<>();
+        this.favouriteRepository.findAll().forEach(Favourite::getBooks);
+        List<Book> searchedBooks = new ArrayList<>();
+        for(Book book: allBooks){
+            if(book.getReviewCalculated().equals(5) && book.getIsRead()){
+                searchedBooks.add(book);
+            }
+        }
+        return searchedBooks;
+    }
+
+    public void deleteBookById(Long id) {
+        boolean exists = favouriteRepository.existsById(id);
+        if(!exists){
+            throw new IllegalStateException("book with id " + id + " does not exists!");
+        }
+        favouriteRepository.deleteById(id);
+    }
 }
